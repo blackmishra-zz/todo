67a3c5ba481c6bc0b7acffd9587e5c5a70ef437d
Shashank <mishra.shashank9598@gmail.com>
2021-06-07 08:54:09 +0530

    CRUD done with db

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -22,12 +22,55 @@ class Todo(db.Model):
 # Application routes
 @app.route("/")
 def hello_world():
+
     return "Hello, World!"
 
 
-@app.route("/home")
+# Create Todo Item
+@app.route("/home", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    todo = Todo()
+    if request.method == "POST":
+        # Form fields
+        title = request.form["title"]
+        desc = request.form["desc"]
+
+        # Sending data to DB
+        data = Todo(title=title, desc=desc)
+        db.session.add(data)
+        db.session.commit()
+
+    display_records = todo.query.all()
+
+    return render_template("index.html", data=display_records)
+
+
+# Update Todo Item
+@app.route("/update/<int:id>", methods=["GET", "POST"])
+def update(id):
+
+    if request.method == "POST":
+        # Form fields
+        item = Todo.query.filter_by(sno=id).first()
+        item.title = request.form["title"]
+        item.desc = request.form["desc"]
+
+        # Sending updated data to DB
+        db.session.add(item)
+        db.session.commit()
+        return redirect("/home")
+
+    item = Todo.query.filter_by(sno=id).first()
+    return render_template("update.html", data=item)
+
+
+# Delete Todo Item
+@app.route("/delete/<int:id>")
+def delete(id):
+    item = Todo.query.filter_by(sno=id).first()
+    db.session.delete(item)
+    db.session.commit()
+    return redirect("/home")
 
 
 # App Starter code
